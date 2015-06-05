64d01fb235ce5d322d15b46a8e9cb4b62229b2fc
Juan P Daza P <juan.daza@er-technology.net>
2015-06-04 19:45:11 -0500

    Added search artist layout

--- app/src/main/java/net/ertechnology/myspoti/MainActivityFragment.java
+++ app/src/main/java/net/ertechnology/myspoti/MainActivityFragment.java
@@ -6,11 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -28,9 +32,23 @@ public class MainActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         ListView listView = (ListView) view.findViewById(R.id.main_listview);
 
-        String[] myData = {"data1", "data2", "data3"};
-        List<String> myList = new ArrayList<String>(Arrays.asList(myData));
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_data, R.id.list_item_description, myData);
+        ArrayList<Map<String, String>> data = new ArrayList<Map<String, String>>();
+        HashMap<String, String> map = new HashMap<>();
+        HashMap<String, String> map2 = new HashMap<>();
+        map.put("image", "image1");
+        map.put("description", "Soul Fly");
+        map2.put("image", "image2");
+        map2.put("description", "Ramones");
+        data.add(map);
+        data.add(map2);
+        String[] from = new String[] {"image", "description"};
+        int[] to = new int[]{R.id.list_item_image, R.id.list_item_description};
+        SimpleAdapter arrayAdapter = new SimpleAdapter(getActivity(), data, R.layout.list_item_data, from, to);
+
+        //String[] myData = {"data1", "data2", "data3"};
+        //List<String> myList = new ArrayList<String>(Arrays.asList(myData));
+        //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_data, R.id.list_item_description, myData);
+
         /*ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_data, R.id.list_item_textview);
         arrayAdapter.add("mango");
         arrayAdapter.add("manzana");
