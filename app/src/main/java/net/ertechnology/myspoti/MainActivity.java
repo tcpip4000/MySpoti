350bd86a8438724f522809690daf557d7055a500
Juan P. Daza P <juan.daza@er-technology.net>
2015-06-08 15:48:33 -0500

    frontend, fixing api connection

--- app/src/main/java/net/ertechnology/myspoti/MainActivity.java
+++ app/src/main/java/net/ertechnology/myspoti/MainActivity.java
@@ -1,17 +1,38 @@
 package net.ertechnology.myspoti;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Album;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE = 1337;
+    private static final String REDIRECT_URI = "yourcustomprotocol://callback";
+    private static final String CLIENT_ID = "22837230eb5045ba9826a6542c1c8169";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getData(this);
 
         getFragmentManager().beginTransaction().add(R.id.main_container, new MainActivityFragment())
                 .commit();
@@ -39,5 +60,63 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static void getData(Activity activity) {
+        /// Auth
+
+/*        AuthenticationRequest.Builder builder =
+                new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
+        builder.setScopes(new String[]{"streaming"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(activity, REQUEST_CODE, request);*/
+        ///
+
+/*        SpotifyApi api = new SpotifyApi();
+        //api.setAccessToken("22837230eb5045ba9826a6542c1c8169:c79858f956ad470aa3f59fa1fc0d214a");
+
+        SpotifyService spotify = api.getService();
+        spotify.getAlbum("2dIGnmEIy1WZIcZCFSj6i8", new Callback<Album>() {
+            @Override
+            public void success(Album album, Response response) {
+                Log.d(LOG_TAG, album.name);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Log.d(LOG_TAG, error.toString());
+            }
+        });*/
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        Log.d(LOG_TAG, "xxx callback");
+
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    Log.d(LOG_TAG, "token");
+                    // Handle successful response
+                    break;
+
+                // Auth flow returned an error
+                case ERROR:
+                    Log.d(LOG_TAG, "error");
+                    // Handle error response
+                    break;
+
+                // Most likely auth flow was cancelled
+                default:
+                    Log.d(LOG_TAG, "default");
+                    // Handle other cases
+            }
+        }
     }
 }
