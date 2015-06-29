9fb01e03d6f0f7875f63f60aa28cc0aaddad0b35
Juan P. Daza P <juan.daza@er-technology.net>
2015-06-29 13:29:03 -0500

    fixing filter

--- app/src/main/java/net/ertechnology/myspoti/MainActivityFragment.java
+++ app/src/main/java/net/ertechnology/myspoti/MainActivityFragment.java
@@ -3,17 +3,19 @@ package net.ertechnology.myspoti;
 import android.app.Fragment;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Filter;
 import android.widget.ListView;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
-import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
-import kaaes.spotify.webapi.android.models.Pager;
 
 
 /**
@@ -41,7 +43,7 @@ public class MainActivityFragment extends Fragment implements AsyncResponse {
 
         GetDataTask getdataTask = new GetDataTask();
         getdataTask.delegate = this;
-        getdataTask.execute(mSpotify);
+        getdataTask.execute("loca");
 
         return view;
     }
@@ -49,32 +51,55 @@ public class MainActivityFragment extends Fragment implements AsyncResponse {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        EditText search = (EditText) getView().findViewById(R.id.main_search);
+        search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+/*                GetDataTask getdataTask = new GetDataTask();
+                getdataTask.delegate = (AsyncResponse) getActivity().getFragmentManager().findFragmentById(R.id.main_container);
+                getdataTask.execute(s.toString());*/
+
+                Filter filter = mCustomAdapter.getFilter();
+                filter.filter(s.toString());
+                Log.d("s is: ", s.toString());
+            }
+        });
     }
 
     @Override
     public void processFinish(ArtistsPager artistsPager) {
-        Pager<Artist> artists = artistsPager.artists;
+/*        Pager<Artist> artists = artistsPager.artists;
         for (Artist artist : artists.items) {
             Log.d(LOG_TAG, "name: " + artist.name);
             Log.d(LOG_TAG, "followers: " + artist.popularity);
             if (artist.images.size() > 0) {
                 Log.d(LOG_TAG, "image0: " + artist.images.get(0).url);
             }
-        }
+        }*/
         ListView listView = (ListView) getView().findViewById(R.id.main_listview);
         mCustomAdapter = new MySpotiAdapter(getActivity(), artistsPager.artists.items);
         listView.setAdapter(mCustomAdapter);
     }
 
 
-    private class GetDataTask extends AsyncTask<SpotifyService, Void, ArtistsPager> {
+    private class GetDataTask extends AsyncTask<String, Void, ArtistsPager> {
 
         public AsyncResponse delegate;
 
         @Override
-        protected ArtistsPager doInBackground(SpotifyService... params) {
-            SpotifyService spotify = params[0]; // TODO make attribute
-            ArtistsPager pager = spotify.searchArtists("loco");
+        protected ArtistsPager doInBackground(String... params) {
+            String searchString = params[0];
+            ArtistsPager pager = mSpotify.searchArtists(searchString);
             return pager;
         }
 
--- app/src/main/java/net/ertechnology/myspoti/MySpotiAdapter.java
+++ app/src/main/java/net/ertechnology/myspoti/MySpotiAdapter.java
@@ -5,23 +5,32 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import kaaes.spotify.webapi.android.models.Artist;
-import kaaes.spotify.webapi.android.models.ArtistsPager;
 
 /**
  * Created by Juan on 29/06/2015.
  */
-public class MySpotiAdapter extends ArrayAdapter<Artist> {
+public class MySpotiAdapter extends ArrayAdapter<Artist> implements Filterable {
+
+    private CustomFilter mFilter;
+    private List<Artist> mObjects;
+    private List<Artist> mOriginalValues;
 
     public MySpotiAdapter(Context context, List<Artist> objects) {
         super(context, 0, objects);
+        mOriginalValues = objects;
+        mObjects = objects;
     }
 
     @Override
@@ -40,4 +49,89 @@ public class MySpotiAdapter extends ArrayAdapter<Artist> {
         return view;
     }
 
+    @Override
+    public int getCount() {
+        return mObjects.size();
+    }
+
+    @Override
+    public Artist getItem(int position) {
+        return mObjects.get(position);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Filter getFilter() {
+
+        if (mFilter == null) {
+            mFilter = new CustomFilter();
+        }
+        return mFilter;
+    }
+
+
+    private class CustomFilter extends Filter {
+
+        /**
+         * <p>Invoked in a worker thread to filter the data according to the
+         * constraint. Subclasses must implement this method to perform the
+         * filtering operation. Results computed by the filtering operation
+         * must be returned as a {@link FilterResults} that
+         * will then be published in the UI thread through
+         * {@link #publishResults(CharSequence,
+         * FilterResults)}.</p>
+         * <p/>
+         * <p><strong>Contract:</strong> When the constraint is null, the original
+         * data must be restored.</p>
+         *
+         * @param constraint the constraint used to filter the data
+         * @return the results of the filtering operation
+         * @see #filter(CharSequence, FilterListener)
+         * @see #publishResults(CharSequence, FilterResults)
+         * @see FilterResults
+         */
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            List<Artist> filteredList = new ArrayList<>();
+            String constraintLower = constraint.toString().toLowerCase();
+            if (constraint!= null && constraint.toString().length() > 0) {
+                for (Artist artist : mOriginalValues) {
+                    if (artist.name.toLowerCase().contains(constraintLower)) {
+                        filteredList.add(artist);
+                    }
+                }
+                results.count = filteredList.size();
+
+            } else {
+                results.count = 0;
+            }
+
+            results.values = filteredList;
+            return results;
+        }
+
+        /**
+         * <p>Invoked in the UI thread to publish the filtering results in the
+         * user interface. Subclasses must implement this method to display the
+         * results computed in {@link #performFiltering}.</p>
+         *
+         * @param constraint the constraint used to filter the data
+         * @param results    the results of the filtering operation
+         * @see #filter(CharSequence, FilterListener)
+         * @see #performFiltering(CharSequence)
+         * @see FilterResults
+         */
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mObjects = (List<Artist>) results.values;
+            if (results.count > 0) {
+                notifyDataSetChanged();
+            } else {
+                notifyDataSetInvalidated();
+            }
+        }
+    }
 }
