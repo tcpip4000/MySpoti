d0c77053ec8ee6e39957c545688930c375c261e6
Juan P Daza P <juan.daza@er-technology.net>
2015-08-18 10:03:31 -0500

    Adding dialog

--- app/src/main/java/net/ertechnology/myspoti/PlayerActivity.java
+++ app/src/main/java/net/ertechnology/myspoti/PlayerActivity.java
@@ -1,18 +1,17 @@
 package net.ertechnology.myspoti;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class PlayerActivity extends AppCompatActivity {
+public class PlayerActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
